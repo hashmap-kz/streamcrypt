249167ff755278f80326c6d5d86773e64d975e91
alexey.zh <mailbox.stdio@gmail.com>
2025-05-08 18:52:54 +0500

    Init repository with core modules.

---
+++ pkg/codec/codec.go
@@ -0,0 +1,39 @@
+package codec
+
+import "io"
+
+const (
+	GzipFileExt = ".gz"
+)
+
+type Flusher interface {
+	Flush() error
+}
+
+type WriteFlushCloser interface {
+	io.WriteCloser
+	Flusher
+}
+
+type Compressor interface {
+	NewWriter(writer io.Writer) (WriteFlushCloser, error)
+	FileExtension() string
+	Name() string
+}
+
+type Decompressor interface {
+	Decompress(src io.Reader) (io.ReadCloser, error)
+	FileExtension() string
+}
+
+func GetDecompressor(c Compressor) Decompressor {
+	if c == nil {
+		return nil
+	}
+	switch c.FileExtension() {
+	case GzipFileExt:
+		return &GzipDecompressor{}
+	default:
+		return nil
+	}
+}
